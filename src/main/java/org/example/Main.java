5924fad79fa4f0eb9f53b715dcdff6f974acc0ab
Ilya Isaev <ilia92isaev@gmail.com>
2024-02-05 17:13:48 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build()) {
+            HttpGet request = new HttpGet(
+                    "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats"
+            );
+            request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+            try (CloseableHttpResponse response = httpClient.execute(request)) {
+                List<Cat> cats = mapper.readValue(
+                        response.getEntity().getContent(),
+                        new TypeReference<>(){});
+                cats.stream()
+                        .filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0)
+                        .forEach(System.out::println);
+            } catch (IOException e) {
+                e.getMessage();
+            }
+        } catch (IOException e) {
+            e.getMessage();
+        }
+    }
+}
